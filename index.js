7d3e36a5059d58110d51c6f01465eacc5f5ffc32
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-07-17 18:40:52 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory")
+
+document.getElementById("victory");
+
+newHeader.innerHTML = "JAMES KAMAU is the champion";
